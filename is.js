6b18f9345aeacaa8af1d69713d9dc73246c8937d
OneCreatorX-New <cuentapanama19@gmail.com>
2024-06-09 10:20:56 -0300

    Update is.js

--- is.js
+++ is.js
@@ -63,15 +63,16 @@ function mostrarScripts() {
       const divAnuncio = document.createElement("div");
       divAnuncio.classList.add("anuncios");
       divAnuncio.innerHTML = `
-        <ins class="adsbygoogle"
-             style="display:block"
-             data-ad-client="ca-pub-6026238594380398"
-             data-ad-slot="2569100541"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-        <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6026238594380398"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="autorelaxed"
+     data-ad-client="ca-pub-6026238594380398"
+     data-ad-slot="2569100541"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
       `;
       contenedorScripts.appendChild(divAnuncio);
     }
