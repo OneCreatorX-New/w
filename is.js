ae793a3433e8a9c39c6480bc8d5e44cfd916450e
OneCreatorX-New <cuentapanama19@gmail.com>
2024-06-09 21:13:56 -0300

    Update is.js

--- is.js
+++ is.js
@@ -7,11 +7,6 @@ const infoPagina = document.getElementById("info-pagina");
 const filtroTodosBtn = document.getElementById("filtro-todos");
 const filtroUniversalesBtn = document.getElementById("filtro-universales");
 const filtroJuegosBtn = document.getElementById("filtro-juegos");
-const discordLinkBtn = document.getElementById("discord-link");
-const youtubeLinkBtn = document.getElementById("youtube-link");
-const mostrarRecientesBtn = document.getElementById("mostrar-recientes");
-const mostrarAntiguosBtn = document.getElementById("mostrar-antiguos");
-const enviarMensajeBtn = document.getElementById("enviar-mensaje");
 
 let scriptsMostrados = 0;
 let scriptsPorPagina = 5;
@@ -179,61 +174,6 @@ filtroJuegosBtn.addEventListener("click", () => {
     paginaActual = 0;
     mostrarScripts();
 });
-
-discordLinkBtn.addEventListener("click", () => {
-    window.open("https://discord.com/invite/qfRWu3pGXm", "_blank");
-});
-
-youtubeLinkBtn.addEventListener("click", () => {
-    window.open("https://youtube.com/@onecreatorx", "_blank");
-});
-
-mostrarRecientesBtn.addEventListener("click", () => {
-    scripts = scriptsOriginales.reverse();  // Mostramos los más recientes primero
-    paginaActual = 0;
-    mostrarScripts();
-});
-
-mostrarAntiguosBtn.addEventListener("click", () => {
-    scripts = scriptsOriginales.reverse();  // Mostramos los más antiguos primero
-    paginaActual = 0;
-    mostrarScripts();
-});
-
-enviarMensajeBtn.addEventListener("click", () => {
-    const mensaje = prompt("Introduce tu mensaje:");
-    if (mensaje) {
-        enviarMensajeDiscord(mensaje);
-    }
-});
-
-async function enviarMensajeDiscord(mensaje) {
-    const webhookURL = "https://discord.com/api/webhooks/1249511240498286632/fjhJy1ZwXO1eEEazsY80ME2FzaOMEEMkYT4IcZSzp76TYAcbaDnnY5BcLXqNOENJeJ7x";
-    
-    const payload = {
-        content: mensaje
-    };
-
-    try {
-        const response = await fetch(webhookURL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(payload)
-        });
-
-        if (response.ok) {
-            alert("Mensaje enviado correctamente.");
-        } else {
-            console.error("Error al enviar el mensaje:", response.statusText);
-            alert("Hubo un error al enviar el mensaje.");
-        }
-    } catch (error) {
-        console.error("Error al enviar el mensaje:", error);
-        alert("Hubo un error al enviar el mensaje.");
-    }
-}
 
 iniciar();
 
